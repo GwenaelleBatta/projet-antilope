bf9878ab0a7fc610fb4b7123b7e9082085576aac
Gwen <gwenaelle.batta@studetn.hepl.be>
2022-05-28 11:11:10 +0200

    Begin media queries

--- wp-content/themes/ecosphair/index.php
+++ wp-content/themes/ecosphair/index.php
@@ -12,7 +12,7 @@
 	</section>
 	<section aria-labelledby="help" class="layout__help help">
 		<div class="help__content">
-			<h2 id="help" class="help__title" aria-level="2"><?= __('En quoi ', 'ecosphair') ?><?= get_bloginfo('name'); ?> <?= __(' peut-elle vous aider ?', 'ecosphair') ?></h2>
+			<h2 id="help" class="help__title" aria-level="2"><?= __('En quoi ', 'ecosphair') ?><?= get_bloginfo('name'); ?> <?= __(' peut vous aider ?', 'ecosphair') ?></h2>
 			<p class="help__text"><?= __('"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?', 'ecosphair') ?>
 			</p>
 			<a href="<?= get_the_permalink(ecosphair_get_template_page('template-about')) ?>"
