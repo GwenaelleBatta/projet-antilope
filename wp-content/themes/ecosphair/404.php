48421b1427d175a58c4321f53ab936cc30de87db
Gwen <gwenaelle.batta@studetn.hepl.be>
2022-06-01 09:02:52 +0200

    CSS page 404 finish

--- wp-content/themes/ecosphair/404.php
+++ wp-content/themes/ecosphair/404.php
@@ -5,11 +5,11 @@
 get_header(); ?>
 <main class="error404">
     <section aria-labelledby="error404" class="error404__container">
-        <div>
+        <div class="error404__position">
             <h2 class="error404__title" id="error404" aria-level="2">404</h2>
-            <h3 class="error404__subtitle"><?= __('Page non trouvée', 'ecosphair') ?></h3>
+            <p class="error404__subtitle"><?= __('Page non trouvée', 'ecosphair') ?></p>
         </div>
-        <p class="error404__content"><?= str_replace(':accueil', '<a href="' . home_url() . '" class="error404__link">' . __('l‘accueil', 'ecosphair') . '</a>', __('Retourner à :accueil', 'ecosphair')) ?></p>
+        <p class="error404__content"><?= str_replace(':accueil', '<a href="' . home_url() . '" class="error404__link">' . __('l’accueil', 'ecosphair') . '</a>', __('Retourner à :accueil', 'ecosphair')) ?></p>
     </section>
 </main>
 <?php get_footer(); ?>
